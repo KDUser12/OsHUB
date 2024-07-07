2f49e8ddd8488bd2cbcede7335feabe5261ac69b
KDUser <lucas.29.deneffe@gmail.com>
2024-07-07 18:33:22 +0200

    Switch versioning plugin
    - Removed - Python version previous to 3.8.x.
    - Compatibility with new versions of Python (3.13).

--- doxhub/__init__.py
+++ doxhub/__init__.py
@@ -4,3 +4,11 @@ This module contains a list of tools and sites to find personal
 information about a target person.
 
 """
+
+from utils.version import get_version
+
+VERSION = (2, 0, 0, 'beta', 0)
+
+__shortname__   = "DoxHub"
+__longname__    = "DoxHub: Identify tools to find personal information."
+__version__     = get_version(VERSION)
--- doxhub/utils/version.py
+++ doxhub/utils/version.py
@@ -1,84 +1,118 @@
+import datetime
+import functools
+import os.path
 import subprocess
 import sys
-import os
-import functools
-import datetime
 
-# Private, stable API for detecting the Python implementation.
+# Private stable API for detecting the Python implementation.
 PYPY = sys.implementation.name == "pypy"
 
-# Private and stable API to detect Python version. PYXY means "Python X.Y". For third-party applications to use these
-# values, each constant must remain supported by Quantum Compiler versions
-PY36 = sys.version_info >= (3, 6)
-PY37 = sys.version_info >= (3, 7)
+# Private, stable API for detecting the Python version. PYXY means "Python X.Y
+# or later". So that third-party apps can use these values, each constant
+# should remain as long as the oldest supported DoxHub version supported that
+# Python version.
 PY38 = sys.version_info >= (3, 8)
 PY39 = sys.version_info >= (3, 9)
 PY310 = sys.version_info >= (3, 10)
 PY311 = sys.version_info >= (3, 11)
 PY312 = sys.version_info >= (3, 12)
+PY313 = sys.version_info >= (3, 13)
 
 
 def get_version(version=None):
-    """Return a PEP 440-compliant version number from VERSION."""
+    """PEP 440-compliant version number
+
+    Build of the two parts of the version number:
+    main = X.Y[.Z]
+    sub = .devN - for pre-alpha releases
+        | {a|b|rc}N - for alpha, beta and rc releases
+
+    Keyword Arguments:
+    :param version:             -- Tuple containing the module version.
+
+    :return:
+    Return a PEP 440-compliant version number from VERSION.
+    """
+
     version = get_complete_version(version)
-
-    # Now build the two parts of the version number:
-    # main = X.Y[.Z]
-    # sub = .devN - for pre-alpha releases
-    #   | {a|b|rc}N - for alpha, beta and rc releases
-
     main = get_main_version(version)
-
     sub = ""
-    if version[3] == "alpha" and version[4] == 0:
+
+    if version[3] == 'alpha' and version[4] == 0:
         git_changeset = get_git_changeset()
         if git_changeset:
             sub = ".dev%s" % git_changeset
-
-    elif version[3] != "final":
-        mapping = {"alpha": "a", "beta": "b", "rc": "rc"}
+    elif version[3] != "release":
+        mapping = {'alpha': 'a', 'beta': 'b', 'rc': 'rc'}
         sub = mapping[version[3]] + str(version[4])
 
     return main + sub
 
 
 def get_main_version(version=None):
-    """Return main version (X.Y[.Z]) from VERSION."""
+    """Build main version.
+
+    Return main version (X.Y[.Z]) from VERSION.
+
+    Keyword Arguments:
+    :param version:             -- Tuple containing the module version.
+
+    :return:
+    Return main version.
+    """
+
     version = get_complete_version(version)
     parts = 2 if version[2] == 0 else 3
+
     return ".".join(str(x) for x in version[:parts])
 
 
 def get_complete_version(version=None):
-    """Return a tuple of the Quantum Compiler version. If version argument is non-empty,
-    check for correctness of the tuple provided.
+    """Checking the tuple.
+
+    If version argument is non-empty, check for correctness of
+    the tuple provided.
+
+    Keyword Arguments:
+    :param version:             -- Tuple containing the module version.
+
+    :return:
+    Return a tuple of the DoxHub version.
     """
-    if version is not None:
+
+    if version is None:
+        from doxhub import VERSION as version
+    else:
         assert len(version) == 5
-        assert version[3] in ('alpha', 'beta', 'rc', 'final')
+        assert version[3] in ('alpha', 'beta', 'rc', 'release')
 
     return version
 
 
 @functools.lru_cache
 def get_git_changeset():
-    """Return a numeric identifier of the latest git changeset.
+    """Generating of development version numbers.
 
     The result is the UTC timestamp of the changeset in YYYYMMDDHHMMSS format.
     This value isn't guaranteed to be unique, but collisions are very unlikely,
     so it's sufficient for generating the development version numbers.
+
+    :return:
+    Return a numeric identifier of the latest git changeset.
     """
-    # Repository may not be found if __file__ is undefined, e.g. in a frozen module.
+
     if "__file__" not in globals():
         return None
+
     repo_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     git_log = subprocess.run(
-        "git log --pretty=format:%ct -- quiet -1 HEAD",
+        "git log --pretty=format:%ct --quiet -1 HEAD",
         capture_output=True,
         shell=True,
         cwd=repo_dir,
         text=True
     )
+
     timestamp = git_log.stdout
     tz = datetime.timezone.utc
 
@@ -86,4 +120,5 @@ def get_git_changeset():
         timestamp = datetime.datetime.fromtimestamp(int(timestamp), tz=tz)
     except ValueError:
         return None
+
     return timestamp.strftime("%Y%m%d%H%M%S")
